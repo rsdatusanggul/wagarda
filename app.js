67fafb62ba31f603389bfa7c2b9f08b5ec191e1b
rsdatusanggul <rsdatusanggul@gmail.com>
2026-02-25 16:12:35 +0800

    fix(cors): add DELETE method to CORS allowed methods for device management

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    methods: ['GET', 'POST', 'OPTIONS'],
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 };
